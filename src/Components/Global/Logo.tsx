ee5dc6215e997688526aeda3884cf60511001f6a
Ivan Grginov <ivangrginovbiz@gmail.com>
2023-09-14 12:57:08 +0200

    Change icon to cat

--- src/Components/Global/Logo.tsx
+++ src/Components/Global/Logo.tsx
@@ -1,12 +1,17 @@
 import { useColorMode } from "@chakra-ui/react";
+import Link from "next/link";
 
-export const Logo = ({ size = 42 }) => {
+export const Logo = () => {
   const { colorMode } = useColorMode();
 
-  const defaultRowColor = colorMode === "dark" ? "var(--chakra-colors-green-100)" : "var(--chakra-colors-green-900)";
-  const highlightedRowColor = colorMode === "dark" ? "var(--chakra-colors-green-400)" : "var(--chakra-colors-green-400)";
+  const defaultRowColor =
+    colorMode === "dark"
+      ? "var(--chakra-colors-green-100)"
+      : "var(--chakra-colors-green-900)";
+  const highlightedRowColor =
+    colorMode === "dark"
+      ? "var(--chakra-colors-green-400)"
+      : "var(--chakra-colors-green-400)";
 
-  return (
-		'😬'
-  );
+  return <Link href="/">😸</Link>;
 };
