18cb4a69a2799d322c46678be608be7333b197e6
Ivan Grginov <ivangrginovbiz@gmail.com>
2023-09-15 13:14:20 +0200

    Add some quick demos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 "use client";
-import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import { Button, Flex, Heading, Image, Text, VStack } from "@chakra-ui/react";
 import DefaultLayout from "@/Layouts/DefaultLayout";
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 import { Icon } from "@chakra-ui/icons";
 import { PageHeader } from "@/Components/Global/PageHeader";
+import { addBadge, swapFavicon, swapTitle, marqueeTitle, resetFavicon, resetTitle } from "tabky-js";
 
 export default function Homepage() {
   return (
     <DefaultLayout justifyContent="center">
-      <PageHeader title={"😸 Tabky.js"} hasDivider={false}>
+      <PageHeader title={"😸 Tabky.js"}>
         <Text maxW="xl">
-          {"A JS library for programmatically manipulating your website's browser tab (favicon / title)."}
+          {
+            "A JS library for programmatically manipulating your website's browser tab (favicon / title)."
+          }
         </Text>
 
         <Flex gap={2}>
@@ -28,6 +31,93 @@ export default function Homepage() {
           </Button>
         </Flex>
       </PageHeader>
+      <VStack marginTop={8} justifyContent={"center"} spacing={4}>
+				<Heading as="h2" marginBottom={4}>Quick demo</Heading>
+        <Flex gap={4} justifyContent={"center"} wrap={"wrap"}>
+          <Button
+            onClick={() => swapFavicon({ favicon: "🎉" })}
+            variant={"outline"}
+						colorScheme="gray"
+          >
+            Swap favicon to 🎉
+          </Button>
+          <Button
+            onClick={() => swapFavicon({ favicon: "https://nextjs.org/favicon.ico" })}
+            variant={"outline"}
+						colorScheme="gray"
+          >
+            Swap favicon to img <Image marginLeft={2} src="https://nextjs.org/favicon.ico" alt="Example favicon image, next favicon." width={"16px"} height={"16px"} />
+          </Button>
+          <Button
+            onClick={() => addBadge({ type: "dot" })}
+            variant={"outline"}
+						colorScheme="gray"
+          >
+            Add badge 🔴
+          </Button>
+          <Button
+            onClick={() => addBadge({ type: "count", count: 1 })}
+            variant={"outline"}
+						colorScheme="gray"
+          >
+            Add badge with count 1
+          </Button>
+          <Button
+            onClick={() => swapTitle({ title: "New page title" })}
+            variant={"outline"}
+						colorScheme="gray"
+          >
+            Swap title
+          </Button>
+          <Button
+            onClick={() => {
+              swapFavicon({ favicon: "🎉", reset: "after" });
+              swapTitle({ title: "Congratz!", reset: "after" });
+            }}
+            variant={"outline"}
+						colorScheme="gray"
+          >
+            Swap favicon & title for 3s
+          </Button>
+          <Button
+            onClick={() => {
+              swapTitle({ title: "Hey, come back", when: "onblur", reset: 'onfocus' });
+              swapFavicon({ favicon: '😢', when: "onblur", reset: 'onfocus' });
+            }}
+            variant={"outline"}
+						colorScheme="gray"
+          >
+            Swap title + favicon on blur, reset on focus
+          </Button>
+          <Button
+            onClick={() =>
+              marqueeTitle({
+                title: "BREAKING NEWS! You can now marquee your title",
+              })
+            }
+            variant={"outline"}
+						colorScheme="gray"
+          >
+            Marquee title
+          </Button>
+        </Flex>
+        <Flex gap={4} justifyContent={"center"}>
+          <Button
+            onClick={() => resetFavicon()}
+            colorScheme="red"
+            variant={"outline"}
+          >
+            Reset Favicon
+          </Button>
+          <Button
+            onClick={() => resetTitle()}
+            colorScheme="red"
+            variant={"outline"}
+          >
+            Reset title
+          </Button>
+        </Flex>
+      </VStack>
     </DefaultLayout>
   );
 }
