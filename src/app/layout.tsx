f235bd47dfa1e6c6556143de573838ddaa52b1da
Ivan Grginov <41570713+MetarDev@users.noreply.github.com>
2023-09-16 13:20:58 +0200

    MVP Polish (#4)
    * Update readme
    
    * Add metadata tags
    
    * Update tabky-js to 1.0.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,32 @@
-import { Header } from "@/Components/Global/Header";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title = 'Tabky.js';
+const description = "A documentation website for Tabky.js library";
+
 export const metadata = {
-	title: "Tabky.js",
-	description: "A documentation website for tabky-js library",
+	title: title,
+	description: description,
+	metadataBase: new URL('https://tabky.dev'),
+	openGraph: {
+    title: title,
+    description: description,
+    url: 'https://tabky.dev',
+    siteName: title,
+    images: '/featured-image.jpg',
+    locale: 'en_US',
+    type: 'website',
+  },
+	twitter: {
+    card: 'summary_large_image',
+    title: title,
+    description: description,
+    creator: '@MeterDev',
+    images: ['featured-image.jpg'],
+  },
 };
 
 export default function RootLayout({
